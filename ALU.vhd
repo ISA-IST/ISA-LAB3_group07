45df40f49551d8d7bc063d96c184e0d6f9c98ec3
Fava Matteo <s274920@studenti.polito.it>
2020-12-06 15:52:11 +0100

    cambiamenti poco significativi

--- ALU.vhd
+++ ALU.vhd
@@ -1,6 +1,6 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
-use ieee.std_logic_arith.all;
+USE ieee.numeric_std.all;
 
 ENTITY ALU IS
 PORT( DATA_A,DATA_B: IN std_logic_vector(63 downto 0);
@@ -22,21 +22,21 @@ BEGIN
     case CTRL is
       --AND
       when "001" =>  DATA_OUT <= DATA_A and DATA_B;
-                    
+
       --ADD
       when "000" =>  OUT_signed = signed(DATA_A) + signed(DATA_B);
                     DATA_OUT <= std_logic_vector(OUT_signed);
-                   
+
       -- SUB
       when "011" =>  OUT_signed = signed(DATA_A) - signed(DATA_B);
                     DATA_OUT <= std_logic_vector(OUT_signed);
-                    
+
       -- XOR
       when "100" =>  DATA_OUT <= DATA_A xor DATA_B;
-                    
+
       -- BARREL SHIFTER
       when "010" => DATA_OUT <= DATA_A << INT_B;
-                   
+
       when others => DATA_OUT <= (OTHERS => '0');
                       ZERO <= '0';
     end case;
