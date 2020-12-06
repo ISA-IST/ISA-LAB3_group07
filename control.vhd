8cab3085e155dd0f2bd42afaf2b43adf5606af3a
s278190 <s278190@studenti.polito.it>
2020-12-06 16:47:57 +0100

    modified control.vhd added alu_control.vhd

--- control.vhd
+++ control.vhd
@@ -4,12 +4,10 @@ USE ieee.std_logic_1164.all;
 ENTITY control IS
 PORT (
 	OPCODE : IN STD_LOGIC_VECTOR(6 DOWNTO 0);
-  FUNC3  : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
-  FUNC7  : IN STD_LOGIC_VECTOR(6 DOWNTO 0);
-
-	ALU_CTRL : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+	
 	ALU_SRC1, SEL_MUX_JAL_AUIPC,REG_WRITE, BRANCH ,SEL_MUX_ADD_SUM  : OUT STD_LOGIC;
-	MEM_TO_REG : OUT STD_LOGIC_VECTOR(1 DOWNTO 0) --"FINAL MUX",
+	MEM_TO_REG : OUT STD_LOGIC_VECTOR(1 DOWNTO 0); --"FINAL MUX",
+	ALU_OP     : OUT STD_LOGIC_VECTOR(1 DOWNTO 0)
 
 
 );
@@ -18,7 +16,7 @@ END control;
 
 ARCHITECTURE BEH OF control IS
 
-CTRL_GEN : PROCESS(OPCODE,FUNC3)
+CTRL_GEN : PROCESS(OPCODE)
 
            BEGIN
 						 --DEFAULT ASSIGNMENTS
@@ -33,6 +31,7 @@ CTRL_GEN : PROCESS(OPCODE,FUNC3)
 					SEL_MUX_ADD_SUM <= '0'; --ALWAYS ON BEQ
 					MEM_TO_REG      <= "00";
 					SEL_MUX_JAL_AUIPC <= '0'; --ALWAYS ON JAL
+					ALU_OP <= "00"; --ALWAYS ON ADD OPERATION
 
 
 					 CASE(OPCODE) is
@@ -51,7 +50,7 @@ CTRL_GEN : PROCESS(OPCODE,FUNC3)
 							MEM_TO_REG <= "11";
 
 						WHEN "1100011"  =>       --BEQ
-							ALU_CTRL <= "011";
+						  ALU_OP <= "01";
 							BRANCH <= '1';
 
 
@@ -61,39 +60,20 @@ CTRL_GEN : PROCESS(OPCODE,FUNC3)
 							MEM_READ  <= '1';
 							MEM_TO_REG <= "00";
 
+
 						WHEN"0100011"  =>       --SW
 							MEM_WRITE <= '1';
 
 						WHEN "0010011"  =>       --ADDI, ANDI, SRAI
-
              MEM_TO_REG <= "01";
+						 ALU_SRC1 <= '1'; -- ATTENZIONE!!
+             ALU_OP <= "11";
 
-							IF (FUNC3 = "000") then --ADDI
-								ALU_CTRL <= "000";
-								ALU_SRC1 <= '1';
-							elsIF (FUNC3 = "111")
-								ALU_CTRL <= "001"; --ANDI
-							END IF;
-
-							IF (FUNC3 = "101") then --SRAI
-								ALU_CTRL <= "010";
-							END IF;
 
 
 						WHEN "0110011"  =>         --ADD, XOR, SLT
-
-							MEM_TO_REG <= "01";
-
-							IF (FUNC3 = "000") then --ADD
-								ALU_CTRL <= "000";
-							elsIF FUNC3 = "100" then  --ADD
-								ALU_CTRL <= "100";       -- XOR
-							END IF;
-
- 							IF (FUNC3 = "010") then     --SLT
-								ALU_CTRL <= "011"       -- SUBTRACT COMMAND
-
- 							END IF;
+            MEM_TO_REG <= "01";
+            ALU_OP <= "10";
 
 
 
