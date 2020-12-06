17751494f3c952ec0f36f3caa655b8df901027cd
Fava Matteo <s274920@studenti.polito.it>
2020-12-06 16:43:31 +0100

    Senza ENABLE

--- regnbit.vhd
+++ regnbit.vhd
@@ -6,23 +6,23 @@ ENTITY regnbit IS
 	GENERIC ( N : POSITIVE := 2
 				);
 	PORT( D    : IN STD_LOGIC_VECTOR(N-1 downto 0);
-		  CLK, RST_n, ENABLE : IN STD_LOGIC;
+		  CLK, RST_n: IN STD_LOGIC;
 		  Q    : OUT STD_LOGIC_VECTOR(N-1 downto 0)
 		 );
 END regnbit;
 
 ARCHITECTURE behaviour OF regnbit IS
 BEGIN
-	
+
 	PROCESS(CLK, RST_n)
 	BEGIN
 		IF(RST_n='0') THEN
 			Q <= (OTHERS=>'0');
 		ELSIF(CLK'EVENT AND CLK='1') THEN
-			IF(ENABLE='1') THEN
+
 				Q <= D;
-			END IF;
+
 		END IF;
 	END PROCESS;
-	
+
 END behaviour;
