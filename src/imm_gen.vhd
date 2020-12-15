973d09f35fb848f0b686ca0624d0509f53950d7f
Fava Matteo <s274920@studenti.polito.it>
2020-12-15 17:12:22 +0100

    testbench of imm_gen.vhd

--- src/imm_gen.vhd
+++ src/imm_gen.vhd
@@ -11,21 +11,24 @@ END imm_gen;
 
 ARCHITECTURE beh of imm_gen IS
 
-SIGNAL opcode: STD_LOGIC_VECTOR(6 downto 0);
-SIGNAL funct3: STD_LOGIC_VECTOR(2 downto 0);
+
 
 BEGIN
-	
-	opcode <= instr(6 downto 0);
-	funct3 <= instr(14 downto 12); --for OP-IMM
-	
-	PROCESS(opcode, funct3)
+
+
+
+	PROCESS(instr)
+	variable opcode: STD_LOGIC_VECTOR(6 downto 0);
+	variable funct3: STD_LOGIC_VECTOR(2 downto 0);
+
 	BEGIN
-	
+	opcode := instr(6 downto 0);
+	funct3 := instr(14 downto 12); --for OP-IMM
+
 	CASE opcode IS
 		-- OP-IMM
 		WHEN "0010011" =>	-- SRAI
-							IF(funct3 = "101") THEN 
+							IF(funct3 = "101") THEN
 								imm <= "000000000000000000000000000" & instr(24 downto 20);
 							-- ADDI, ANDI
 							ELSE
@@ -40,7 +43,7 @@ BEGIN
 		-- BRANCH BEQ
 		WHEN "1100011" => imm <= instr(31) & instr(31) & instr(31) & instr(31) & instr(31) & instr(31) & instr(31) & instr(31) &
 								instr(31) & instr(31) & instr(31) & instr(31) & instr(31) & instr(31) & instr(31) & instr(31) &
-								instr(31) & instr(31) & instr(31) & instr(31) & instr(31) & instr(7) & instr(30 downto 25) & instr(11 downto 8); 
+								instr(31) & instr(31) & instr(31) & instr(31) & instr(31) & instr(7) & instr(30 downto 25) & instr(11 downto 8);
 		-- LOAD LW
 		WHEN "0000011" => imm <= instr(31) & instr(31) & instr(31) & instr(31) & instr(31) & instr(31) & instr(31) & instr(31) &
 								instr(31) & instr(31) & instr(31) & instr(31) & instr(31) & instr(31) & instr(31) & instr(31) &
@@ -51,12 +54,11 @@ BEGIN
 								instr(31) & instr(31) & instr(31) & instr(31) & instr(31 downto 25) & instr(11 downto 7);
 		-- JAL
 		WHEN "1101111" => imm <= instr(31) & instr(31) & instr(31) & instr(31) & instr(31) & instr(31) & instr(31) & instr(31) &
-								instr(31) & instr(31) & instr(31) & instr(31) & instr(31) & instr(18 downto 11) & instr(19) & instr(30 downto 21);
-								
-		WHEN OTHERS => imm <= (OTHERS => '0');				
-								
+								instr(31) & instr(31) & instr(31) & instr(31) & instr(31) & instr(19 downto 12) & instr(20) & instr(30 downto 21);
+
+		WHEN OTHERS => imm <= (OTHERS => '0');
+
 	END CASE;
 	END PROCESS;
-	
+
 END beh;
-								
