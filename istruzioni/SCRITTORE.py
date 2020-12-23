a511baab555b37579051ec66c604b526cca4666c
s276493 <s276493@studenti.polito.it>
2020-12-23 14:39:19 +0100

    reordering folders and test of reordered code

---
+++ istruzioni/SCRITTORE.py
@@ -0,0 +1,19 @@
+
+file_r = open("code_nop.txt","r")
+file_w = open("im_nop.txt","w")
+
+idx = 4194304 
+line = file_r.readlines()
+
+for i in line:
+    file_w.write('IM(' + str(idx) +') <= "'+ i.rstrip()[-8:]+'";\n')
+    file_w.write('IM(' + str(idx+1) +') <= "'+i.rstrip()[-16:-8]+'";\n')
+    file_w.write('IM(' + str(idx+2) +') <= "'+i.rstrip()[-24:-16]+'";\n')
+    file_w.write('IM(' + str(idx+3) +') <= "'+i.rstrip()[0:8]+'";\n')
+    idx=idx+4
+
+
+file_r.close()
+file_w.close()
+    
+    
