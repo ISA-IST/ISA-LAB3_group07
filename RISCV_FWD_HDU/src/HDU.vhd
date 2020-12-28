a2e562c2131db2cf02c1f2288847e21ea20c3578
Fava Matteo <s274920@studenti.polito.it>
2020-12-28 10:46:13 +0100

    creation of HDU

--- RISCV_FWD_HDU/src/HDU.vhd
+++ RISCV_FWD_HDU/src/HDU.vhd
@@ -4,5 +4,24 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity HDU is
-  port();
+  port(
+  RS1, RS2 , RD_1: IN std_logic_vector(4 downto 0);
+  MEM_READ_1: IN std_logic;
+  ENABLE_PC_ID , SEL_MUX_HDU : OUT std_logic
+  );
 end HDU ;
+
+ARCHITECTURE BEH OF HDU IS
+begin
+
+  process(RS_1,RS_2,RD,MEM_READ_1)
+    begin
+      ENABLE_PC_ID <= '0';
+      SEL_MUX_HDU <= '0';
+      if(MEM_READ_1 = '1' and RS1 = RD_1 or RS2 = RD_1) then
+        ENABLE_PC_ID <= '1';
+        SEL_MUX_HDU <= '1';      
+      end if;
+  end process;
+
+end ARCHITECTURE;
