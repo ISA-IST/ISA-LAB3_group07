385f3dc1419e65a2616096a8e0ad8ee0c6adb4df
S274920 <s27420@studenti.polito.it>
2020-12-29 11:58:03 +0100

    correction abs.

--- RISCV_FWD_HDU/src/Absolute_value.vhd
+++ RISCV_FWD_HDU/src/Absolute_value.vhd
@@ -7,19 +7,20 @@ PORT (
   DATA_IN : IN STD_LOGIC_VECTOR (31 downto 0);
   DATA_OUT : OUT STD_LOGIC_VECTOR (31 downto 0);
   ENABLE : IN STD_LOGIC
-
 );
 END Absolute_value;
 
 ARCHITECTURE beh OF Absolute_value IS
+
 BEGIN
 
   process(DATA_IN, ENABLE)
   begin
     if(ENABLE = '1') then
-        DATA_OUT <= abs(DATA_IN);
+        DATA_OUT <= std_logic_vector(abs(signed(DATA_IN)));
     else
         DATA_OUT <= (OTHERS => '0');
     end if;
  end process;
+
 END ARCHITECTURE;
